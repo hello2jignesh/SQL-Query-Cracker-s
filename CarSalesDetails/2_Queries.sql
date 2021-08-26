a5a05848c6b2358a746fca3f7436f7aafea19102
Jignesh Patel <hello2jignesh@gmail.com>
2021-08-26 16:53:38 +0530

    Added answer to Queries 4,5,6

--- CarSalesDetails/2_Queries.sql
+++ CarSalesDetails/2_Queries.sql
@@ -36,3 +36,52 @@ FROM TAB
 INNER JOIN Sales_Team ST WITH(NOLOCK) ON ST.[Emp No] = TAB.[Emp No]
  
 /* 3) What is the frequency of each car sold? (We want the car name and the frequency) */
+SELECT 
+CD.[Car Name]
+,COUNT(1) AS Frequency
+FROM Cars_Data CD WITH(NOLOCK)
+INNER JOIN Sales_Data SD WITH(NOLOCK) ON SD.[Customer Car Code] = CD.[Car Code]
+GROUP BY CD.[Car Name]
+
+/* 4. Create table structure as per pdf */
+SELECT 
+	ST.[Car Dealer First Name] + ' ' + ST.[Car Dealer Last Name] AS [Car Dealer Full Name]
+	,SD.[Customer Name]
+	,CD.[Car Name]
+	,CD.[Car Price]
+	,SD.[Deposit Paid for booking] AS [Deposit Given]
+	,CD.[Car Price] - SD.[Deposit Paid for booking] AS [Amount Left]
+	FROM Sales_Team ST WITH(NOLOCK)
+INNER JOIN Sales_Data SD WITH(NOLOCK) ON SD.[Customer manager ID] = ST.[Emp No]
+INNER JOIN Cars_Data CD WITH(NOLOCK) ON CD.[Car Code] = SD.[Customer Car Code]
+
+/* 5. For Manager with Employee No. 12134 which car constituted to how much percentage of his target. */
+
+DECLARE @ManagerID INT = 12134
+SELECT 
+	ST.[Car Dealer First Name] + ' ' + ST.[Car Dealer Last Name] AS [Car Dealer Full Name]
+	,CD.[Car Code]
+	,CD.[Car Name] 
+	,ST.[Monthly Target]
+	,CD.[Car Price]
+	,(CD.[Car Price] * 100) / ST.[Monthly Target] AS [Percentage COntributed]
+FROM Sales_Team ST WITH(NOLOCK)
+INNER JOIN Sales_Data SD WITH(NOLOCK) ON SD.[Customer manager ID] = ST.[Emp No]
+INNER JOIN Cars_Data CD WITH(NOLOCK) ON CD.[Car Code] = SD.[Customer Car Code]
+WHERE ST.[Emp No] = @ManagerID
+
+/* 6. What is the minimum amount of deposit collected by each car dealer manager? */
+
+;WITH TAB AS
+(
+SELECT 
+	ST.[Car Dealer First Name] + ' ' + ST.[Car Dealer Last Name] AS [Car Dealer Full Name]
+	,SD.[Deposit Paid for booking] 
+	,ROW_NUMBER() OVER (PARTITION BY ST.[Emp No] ORDER BY SD.[Deposit Paid for booking] DESC ) AS RN
+FROM Sales_Team ST WITH(NOLOCK)
+INNER JOIN Sales_Data SD WITH(NOLOCK) ON SD.[Customer manager ID] = ST.[Emp No] 
+)
+SELECT 
+	[Car Dealer Full Name]
+	,[Deposit Paid for booking] AS [Minimum Amount Collected]
+FROM TAB WHERE RN = 1
